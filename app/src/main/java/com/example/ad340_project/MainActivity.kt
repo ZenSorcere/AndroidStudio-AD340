c515be6cde6f6c4b26ca030f7960503a75fb8b4c
ZenSorcere <zen.sorcere@gmail.com>
2021-04-17 16:14:22 -0700

    constraint updates for HW2 button grid and toasts

--- app/src/main/java/com/example/ad340_project/MainActivity.kt
+++ app/src/main/java/com/example/ad340_project/MainActivity.kt
@@ -3,23 +3,32 @@ package com.example.ad340_project
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    val btnArray = arrayOf("Luke", "Leia", "Han", "Chewie", "R2-D2", "C-3P0")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
     // Kotlin function for a Toast
     // gButton1 is the only one this is implemented on yet.
-    // all other buttons do nothing onClick, but gButton1 makes a noise
-    // But no Toast appears. :/
-    fun showLuke(view: View) {
-    val text = "Luke Skywalker"
-    val duration = Toast.LENGTH_SHORT
 
-    val luke = Toast.makeText(applicationContext, text, duration).show()
+    // need to figure out how to get button id reference universal
+    fun showText(view: View) {
+        //val button: Button = findViewById(R.id.gButton1)
+        val button = view as Button
+        val context = applicationContext
+        val duration = Toast.LENGTH_SHORT
+        val text = button.text
+
+    val btnText = Toast.makeText(context, text, duration).show()
     }
+
 
 }
