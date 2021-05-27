33ef6aa397b9ca36d8c2645f17e90b3172cd6589
ZenSorcere <zen.sorcere@gmail.com>
2021-05-27 16:10:35 -0700

    initial firebase sign in code

--- app/src/main/java/com/gilsoncoding/ad340_project/MainActivity.kt
+++ app/src/main/java/com/gilsoncoding/ad340_project/MainActivity.kt
@@ -1,29 +1,59 @@
 package com.gilsoncoding.ad340_project
 
+import android.R.attr.password
 import android.content.Intent
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.UserProfileChangeRequest
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mNameField : EditText
+    lateinit var mEmailField : EditText
+    lateinit var mPassword : EditText
 
+    lateinit var mPreferences: SharedPreferences
+    var sharedPrefFile : String = "com.gilsoncoding.ad340_project"
 //    val btnArray = arrayOf("Luke", "Leia", "Han", "Chewie", "R2-D2", "C-3P0")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
+        mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE)
+
+        mNameField = findViewById(R.id.editTextTextPersonName)
+        mEmailField = findViewById(R.id.editTextEmail)
+        mPassword = findViewById(R.id.editTextPassword)
+
+        mNameField.setText(mPreferences.getString("name", "nametest"))
+        mEmailField.setText(mPreferences.getString("email", "emailtest"))
+        mPassword.setText(mPreferences.getString("password", "passtest"))
+
 //        val myButton: Button = findViewById(R.id.gButton1)
 //        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, btnArray)
 //        val gridView: GridView = findViewById(R.id.grid1)
 //        gridView.adapter = adapter
-//
 
+    }
+
+    fun logIn(view: View?) {
+        Log.d("FIREBASE", "click")
+        signIn()
     }
     // function to show the DisplayMovies Activity on button click
     fun displayMovies(view: View?) {
@@ -52,6 +82,86 @@ class MainActivity : AppCompatActivity() {
 
         val btntext = Toast.makeText(context, text, duration).show()
     }
+    private fun validateLogin (name: String, email : String, password: String): Boolean {
+        var validated = true
+        if (TextUtils.isEmpty(email)) {
+            mEmailField.error = "Email Required"
+            validated = false
+        } else {
+            mEmailField.error = null
+        }
+        if (TextUtils.isEmpty(password)) {
+            mPassword.error = "Password Required"
+            validated = false
+        } else {
+            mPassword.error = null
+
+        }
+        if (TextUtils.isEmpty(name)) {
+            mNameField.error = "Username Required"
+            validated = false
+        } else {
+            mNameField.error = null
+        }
+        return validated
+    }
+
+    private fun signIn() {
+        Log.d("FIREBASE", "signIn")
+
+        // 1 - validate name, email, and password entries
+        var name : String = mNameField.text.toString()
+        var email : String = mEmailField.text.toString()
+        var password : String = mPassword.text.toString()
+
+
+        if (!validateLogin(name, email, password)) {
+            return
+        }
+
+        // 2 - save valid entries to shared preferences
+
+        mPreferences.edit(true) { putString("name", name)}
+        mPreferences.edit(true) { putString("email", email)}
+        mPreferences.edit(true) { putString("password", password)}
+        /*mSharedPreferencesHelper.saveEntry("email", email);
+        mSharedPreferencesHelper.saveEntry("password", password);*/
+
+        // 3 - sign into Firebase
+        val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    Log.d("FIREBASE", "signIn:onComplete:" + task.isSuccessful)
+                    if (task.isSuccessful) {
+                        // update profile
+                        val user: FirebaseUser? = FirebaseAuth.getInstance().currentUser
+                        val profileUpdates: UserProfileChangeRequest =
+                            UserProfileChangeRequest.Builder()
+                                .setDisplayName(mNameField.text.toString())
+                                .build()
+                        user?.updateProfile(profileUpdates)
+                            ?.addOnCompleteListener { task ->
+                                if (task.isSuccessful) {
+                                    Log.d("FIREBASE", "User profile updated.")
+                                    // Go to FirebaseActivity
+                                    startActivity(
+                                        Intent(
+                                            this@MainActivity,
+                                            FirebaseActivity::class.java
+                                        )
+                                    )
+                                }
+                            }
+                    } else {
+                        Log.d("FIREBASE", "sign-in failed")
+                        Toast.makeText(
+                            this@MainActivity, "Sign In Failed",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+    }
+
 
     companion object {
         const val EXTRA_MESSAGE = "com.gilsoncoding.ad340_project.MESSAGE"
